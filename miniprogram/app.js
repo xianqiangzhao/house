e0e6cc13ff72d0b278518dcd8ca348f67a6da4f2
zhaoxianqiang <24930662@qq.com>
2021-04-18 18:05:50 +0800

    add

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -6,7 +6,7 @@ App({
             console.error('请使用 2.2.3 或以上的基础库以使用云能力')
         } else {
             wx.cloud.init({
-                env: 'dev-house-0tiax',
+                env: 'dev-5g6k4kl1b95abd39',
                 traceUser: true,
             })
         }
